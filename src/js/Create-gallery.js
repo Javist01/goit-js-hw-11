1955d6d23d2e089f38a9e5758932bcde14fc1c09
Vlad <40580001+Javist01@users.noreply.github.com>
2022-10-02 23:44:29 +0300

    .

---
+++ src/js/Create-gallery.js
@@ -0,0 +1,84 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import NewsApiService from './news-api';
+import galleryTpl from './markup/create-markup.hbs';
+// import 'infinite-scroll';
+
+const Handlebars = require('handlebars');
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryList: document.querySelector('.Gallery_list'),
+  buttonLoadMore: document.querySelector('.button'),
+};
+
+const API = new NewsApiService();
+var galleryViewer = new SimpleLightbox('.photo-card a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.buttonLoadMore.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+  clearMarkup();
+
+  API.query = e.currentTarget.elements.searchQuery.value.trim();
+  if (!API.query) {
+    return;
+  }
+
+  API.resetPage();
+  API.fetchImages().then(hits => {
+
+    removeClassHiddenBtn();
+    if (hits.length === 0) {
+      clearMarkup();
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notify.success('Hooray! We found totalHits images');
+    }
+
+    appendPicturesMarkup(hits);
+  });
+}
+
+function onLoadMore(hits) {
+  if (!API.query) {
+    return;
+  }
+
+  API.fetchImages().then(hits => {
+    if (hits.length === 0) {
+      
+      Notify.info(
+        'We are sorry, but you have reached the end of search results.'
+      );
+    }
+    appendPicturesMarkup(hits);
+  });
+}
+
+function appendPicturesMarkup(hits) {
+  refs.galleryList.insertAdjacentHTML('beforeend', galleryTpl(hits));
+  galleryViewer.refresh();
+}
+
+function clearMarkup() {
+  refs.galleryList.innerHTML = '';
+  addClassHiddenBtn();
+}
+
+function addClassHiddenBtn() {
+  refs.buttonLoadMore.classList.add('visually-hidden');
+}
+
+function removeClassHiddenBtn() {
+  refs.buttonLoadMore.classList.remove('visually-hidden');
+}
---
+++ src/js/news-api.js
@@ -0,0 +1,39 @@
+const axios = require('axios').default;
+
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    const URL = 'https://pixabay.com/api/';
+    const KEY = '30097880-73ac2834789f98742941535c7';
+    let searchParams = `per_page=40&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}`;
+    const response = await axios(
+      `${URL}?key=${KEY}&q=${this.searchQuery}&${searchParams}`
+    );
+
+    if (response.status === 200) {
+      return response.data.hits;
+    } else {
+      throw new Error(response.statusText);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newSearchQuery) {
+    this.searchQuery = newSearchQuery;
+  }
+}
